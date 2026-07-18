0bc025a14a18c7de5f10e4c34e2f2d0311bc6861
carlos34535 <carloshsl2012@hotmail.com>
2026-07-17 21:32:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,82 +2,218 @@ let windowsEscolhido = "";
 let arquiteturaEscolhida = "";
 
 
+
+
+// ESCOLHER WINDOWS
+
 function selecionarWindows(windows){
 
-    windowsEscolhido = windows;
 
-    document.getElementById("inicio").style.display = "none";
+windowsEscolhido = windows;
 
-    document.getElementById("arquitetura").style.display = "block";
+
+
+document.getElementById("telaWindows")
+.style.display="none";
+
+
+
+document.getElementById("telaArquitetura")
+.style.display="block";
+
+
 
 }
 
+
+
+
+
+
+// ESCOLHER ARQUITETURA
 
 
 function selecionarArquitetura(tipo){
 
-    arquiteturaEscolhida = tipo;
 
-    document.getElementById("arquitetura").style.display = "none";
+arquiteturaEscolhida = tipo;
 
-    carregarProgramas();
+
+
+document.getElementById("telaArquitetura")
+.style.display="none";
+
+
+
+carregarProgramas();
+
 
 }
 
+
+
+
+
+
+
+
+// BUSCAR PROGRAMAS
 
 
 async function carregarProgramas(){
 
-    const resposta = await fetch("./database/apps.json");
-
-    const programas = await resposta.json();
 
 
-    let lista = "";
+try{
 
 
-    programas.forEach(programa => {
-
-        lista += `
-        
-        <div class="card">
-        ✅ ${programa.nome}
-        <br>
-        <small>${programa.categoria}</small>
-        </div>
-
-        `;
-
-    });
+let resposta = await fetch("./database/apps.json");
 
 
 
-    document.getElementById("resultado").style.display="block";
+let programas = await resposta.json();
 
 
-    document.getElementById("config").innerHTML = `
 
-    Windows:
-    <b>${windowsEscolhido}</b>
+mostrarProgramas(programas);
 
-    <br>
 
-    Arquitetura:
-    <b>${arquiteturaEscolhida}</b>
 
-    <hr>
+}
 
-    ${lista}
+catch(error){
 
-    `;
+
+document.getElementById("resultado")
+.style.display="block";
+
+
+
+document.getElementById("config")
+.innerHTML=
+
+"Erro ao carregar banco de programas";
+
+
+
+console.log(error);
 
 
 }
 
 
 
+}
+
+
+
+
+
+
+
+
+// MOSTRAR PROGRAMAS
+
+
+function mostrarProgramas(programas){
+
+
+
+document.getElementById("resultado")
+.style.display="block";
+
+
+
+let lista="";
+
+
+
+programas.forEach(programa=>{
+
+
+lista += `
+
+
+<div class="item">
+
+
+✅ <b>${programa.nome}</b>
+
+<br>
+
+<small>
+${programa.categoria}
+</small>
+
+
+</div>
+
+
+
+`;
+
+
+
+});
+
+
+
+
+
+
+document.getElementById("config")
+.innerHTML=
+
+
+
+`
+
+Windows:
+<b>${windowsEscolhido}</b>
+
+
+<br><br>
+
+
+Arquitetura:
+<b>${arquiteturaEscolhida}</b>
+
+
+<hr>
+
+
+${lista}
+
+
+`;
+
+
+
+}
+
+
+
+
+
+
+
+// BOTÃO FINAL
+
+
 function gerarPacote(){
 
-alert("Pacote criado!");
+
+
+alert(
+
+"Pacote criado!\n\n"+
+windowsEscolhido+
+"\n"+
+arquiteturaEscolhida
+
+
+);
+
+
 
 }
