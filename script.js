6b69fc915a70f8eacff111081488db6a1f2f6ba1
carlos34535 <carloshsl2012@hotmail.com>
2026-07-17 21:52:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,200 +1,4 @@
-let windowsEscolhido = "";
-let arquiteturaEscolhida = "";
-
-let programasSelecionados = [];
-
-
-
-
-// ESCOLHER WINDOWS
-
-function selecionarWindows(windows){
-
-
-windowsEscolhido = windows;
-
-
-document.getElementById("telaWindows").style.display="none";
-
-
-document.getElementById("telaArquitetura").style.display="block";
-
-
-}
-
-
-
-
-
-// ESCOLHER ARQUITETURA
-
-
-function selecionarArquitetura(tipo){
-
-
-arquiteturaEscolhida = tipo;
-
-
-document.getElementById("telaArquitetura").style.display="none";
-
-
-carregarProgramas();
-
-
-}
-
-
-
-
-
-
-// BUSCAR PROGRAMAS
-
-
-async function carregarProgramas(){
-
-
-try{
-
-
-const resposta = await fetch("./database/apps.json");
-
-
-const programas = await resposta.json();
-
-
-
-document.getElementById("resultado").style.display="block";
-
-
-
-document.getElementById("config").innerHTML=
-
-`
-
-Windows:
-<b>${windowsEscolhido}</b>
-
-<br><br>
-
-Arquitetura:
-<b>${arquiteturaEscolhida}</b>
-
-`;
-
-
-
-
-let lista="";
-
-
-
-programas.forEach((programa,index)=>{
-
-
-lista += `
-
-
-<div class="item">
-
-
-<input 
-type="checkbox"
-id="prog${index}"
-value="${programa.nome}"
-onchange="selecionarPrograma('${programa.nome}')">
-
-
-<label for="prog${index}">
-
-<b>${programa.nome}</b>
-
-<br>
-
-<small>${programa.categoria}</small>
-
-</label>
-
-
-</div>
-
-
-`;
-
-
-});
-
-
-
-document.getElementById("listaProgramas").innerHTML = lista;
-
-
-
-}
-
-catch(error){
-
-
-console.log(error);
-
-
-document.getElementById("listaProgramas").innerHTML=
-
-"Erro ao carregar programas";
-
-
-}
-
-
-
-}
-
-
-
-
-
-
-
-// MARCAR PROGRAMAS
-
-
-function selecionarPrograma(nome){
-
-
-if(programasSelecionados.includes(nome)){
-
-
-programasSelecionados =
-programasSelecionados.filter(
-item => item !== nome
-);
-
-
-}
-
-else{
-
-
-programasSelecionados.push(nome);
-
-
-}
-
-
-}
-
-
-
-
-
-
-
-
-// GERAR PACOTE
-
-
 function gerarPacote(){
-
 
 
 if(programasSelecionados.length === 0){
@@ -210,30 +14,94 @@ return;
 
 
 
-let texto =
+let bat = `@echo off
 
-"PC RESET KIT\n\n"+
+title PC RESET KIT - Instalador
 
-windowsEscolhido+
-"\n"+
-arquiteturaEscolhida+
-"\n\nProgramas:\n\n";
+echo =====================================
+echo       PC RESET KIT
+echo Instalando programas selecionados
+echo =====================================
+
+echo.
+
+`;
 
 
 
-programasSelecionados.forEach(programa=>{
+programasSelecionados.forEach(programa => {
 
 
-texto += "✔ "+programa+"\n";
+if(programa === "Google Chrome"){
+
+bat += `
+echo Instalando Google Chrome...
+winget install Google.Chrome --silent
+`;
+
+}
+
+
+if(programa === "7-Zip"){
+
+bat += `
+echo Instalando 7-Zip...
+winget install 7zip.7zip --silent
+`;
+
+}
+
+
+if(programa === "VLC Player"){
+
+bat += `
+echo Instalando VLC Player...
+winget install VideoLAN.VLC --silent
+`;
+
+}
+
+
+if(programa === "Java"){
+
+bat += `
+echo Instalando Java...
+winget install Oracle.JavaRuntimeEnvironment --silent
+`;
+
+}
+
+
+if(programa === "Visual C++ Redistributable"){
+
+bat += `
+echo Instalando Visual C++...
+winget install Microsoft.VCRedist.2015+.x64 --silent
+`;
+
+}
 
 
 });
 
 
 
+bat += `
+
+echo.
+echo =====================================
+echo INSTALACAO FINALIZADA
+echo =====================================
+
+pause
+`;
+
+
+
+
 let arquivo = new Blob(
-[texto],
-{type:"text/plain"}
+[bat],
+{type:"application/bat"}
 );
 
 
@@ -244,11 +112,10 @@ let link=document.createElement("a");
 link.href=URL.createObjectURL(arquivo);
 
 
-link.download="PC_RESET_KIT.txt";
+link.download="PC_RESET_KIT.bat";
 
 
 link.click();
-
 
 
 }
